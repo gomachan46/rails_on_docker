c897fb10f5fc8a6e946cd341a93def1eb1125521
gomachan46 <shiro.gomachan46@gmail.com>
2018-03-05 03:09:53 +0900

    fix

--- docker/scripts/bundler_waiter.sh
+++ docker/scripts/bundler_waiter.sh
@@ -3,13 +3,8 @@
 source /root/.bashrc
 set -ue
 
-echo "Waiting to complete bundle install..."
-sleep 30
-
 until bundle check --dry-run > /dev/null
 do
   echo "Waiting to complete bundle install..."
   sleep 5
 done
-
-bundle exec rake db:create
